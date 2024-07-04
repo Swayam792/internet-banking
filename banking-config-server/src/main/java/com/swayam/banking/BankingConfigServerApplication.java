4d4d95105fed907fcf30ac310c9884781041082c
INC02694 <157596692+INC02694@users.noreply.github.com>
2024-07-04 22:07:50 +0530

    added config server

--- banking-config-server/src/main/java/com/swayam/banking/BankingConfigServerApplication.java
+++ banking-config-server/src/main/java/com/swayam/banking/BankingConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.swayam.banking;
+  package com.swayam.banking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
